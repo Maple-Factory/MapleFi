977ae2636974aa066be3944a994c2e02d903a17e
Nevaland <deanh0716@gmail.com>
2020-03-24 17:37:31 +0900

    Apply binding view model and switching intent and wifiutil

--- app/src/main/java/com/example/maplefi/MainActivity.java
+++ app/src/main/java/com/example/maplefi/MainActivity.java
@@ -1,17 +1,36 @@
 package com.example.maplefi;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
+import android.content.Intent;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.maplefi.databinding.ActivityMainBinding;
+import com.example.maplefi.ui.MainViewModel;
+import com.example.maplefi.util.MainActivityNavigator;
+import com.example.maplefi.util.WifiUtil;
+
+public class MainActivity extends AppCompatActivity implements MainActivityNavigator {
+    private ActivityMainBinding binding;
+    private MainViewModel model;
+    private WifiUtil wifiUtil ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
+        wifiUtil = new WifiUtil(getApplicationContext(), this);
+        model = new MainViewModel(this, wifiUtil);
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        binding.setModel(model);
+        model.onCreate();
     }
 
+    @Override
+    public void callMoreActivity() {
+        startActivity(new Intent(getApplicationContext(), MoreActivity.class));
 
+    }
 }
