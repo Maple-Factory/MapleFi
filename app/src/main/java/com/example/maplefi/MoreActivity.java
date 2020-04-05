15c078c2ace581328e10b847b43e2aee43a5b6c4
Nevaland <deanh0716@gmail.com>
2020-04-06 07:42:57 +0900

    Implement ApItem and Apply to switching activity

--- app/src/main/java/com/example/maplefi/MoreActivity.java
+++ app/src/main/java/com/example/maplefi/MoreActivity.java
@@ -19,9 +19,10 @@ public class MoreActivity extends AppCompatActivity {
         setContentView(R.layout.activity_more);
 
         Intent intent = getIntent();
+        ApItem ap_item = (ApItem) intent.getSerializableExtra("AP_ITEM");
 
         final TextView textViewSsid = (TextView) findViewById(R.id.tv_ssid);
-        textViewSsid.setText(intent.getStringExtra("AP_NAME"));
+        textViewSsid.setText(ap_item.getSsid());
 
         final ImageView imgRssi = (ImageView) findViewById(R.id.img_rssiDegree);
         int rssi_level = ap_item.getRssiLevel();
@@ -49,9 +50,9 @@ public class MoreActivity extends AppCompatActivity {
         });
 
         final TextView textViewSecScore = (TextView) findViewById(R.id.sec_score_tv);
-        textViewSecScore.setText(intent.getStringExtra("AP_SEC_SCORE"));
+        textViewSecScore.setText(Integer.toString(ap_item.getSecScore()));
         final TextView textViewSpdScore = (TextView) findViewById(R.id.speed_score_tv);
-        textViewSpdScore.setText(intent.getStringExtra("AP_SPEED"));
+        textViewSpdScore.setText(Integer.toString(ap_item.getRssiScore()));
 
     }
 }
