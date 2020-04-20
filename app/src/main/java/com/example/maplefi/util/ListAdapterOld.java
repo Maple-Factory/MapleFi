888b135c932f694950739282c2a474ffca8978f3
Nevaland <deanh0716@gmail.com>
2020-04-21 04:08:23 +0900

    Refactoring

--- app/src/main/java/com/example/maplefi/util/ListAdapterOld.java
+++ app/src/main/java/com/example/maplefi/util/ListAdapterOld.java
@@ -28,7 +28,7 @@ public class ListAdapterOld extends RecyclerView.Adapter<ListAdapterOld.ViewHold
     private ArrayList<ApItem> ap_items = null;
     public OnApItemClickListener OnApItemClickListener;
 
-    //아이템 뷰를 저장하는 뷰 홀더 클래스
+    // 아이템 뷰를 저장하는 뷰 홀더 클래스
     public class ViewHolder extends RecyclerView.ViewHolder {
         TextView textView_SSID;
         ImageView img_ap_rssi;
@@ -37,22 +37,22 @@ public class ListAdapterOld extends RecyclerView.Adapter<ListAdapterOld.ViewHold
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
-            //뷰 객체에 대한 참조
+            // 뷰 객체에 대한 참조
             textView_SSID = itemView.findViewById(R.id.tv_ssid);
             img_ap_rssi = itemView.findViewById(R.id.img_rssiDegree);
             btn_ap_info = itemView.findViewById(R.id.imgb_moreinf);
             btn_ap_connect = itemView.findViewById(R.id.imgb_connect);
 
-            //아이템 클릭 이벤트 처리
+            // 아이템 클릭 이벤트 처리
             itemView.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
                     int pos = getAdapterPosition();
                     if(pos != RecyclerView.NO_POSITION){
-                        //아이템 클릭 이벤트
+                        // 아이템 클릭 이벤트
                         ApItem apItem = ap_items.get(pos);
                         Log.d("TEST", "onClick: this is listAdapter" + pos);
-                        //TODO: 토리 profile 삭제 넣으면 됨
+                        // TODO: 토리 profile 삭제 넣으면 됨
 
                         notifyItemChanged(pos) ;
                     }
@@ -62,7 +62,7 @@ public class ListAdapterOld extends RecyclerView.Adapter<ListAdapterOld.ViewHold
         }
     }
 
-    //생성자에게 리스트 객체 전달 받는 파트
+    // 생성자에게 리스트 객체 전달 받는 파트
     public ListAdapterOld(ArrayList<ApItem> list, OnApItemClickListener onApItemClickListener/*, OnItemClickListener itemClickListener*/){
         this.ap_items = list;
         this.OnApItemClickListener = onApItemClickListener;
@@ -71,7 +71,7 @@ public class ListAdapterOld extends RecyclerView.Adapter<ListAdapterOld.ViewHold
 
     @NonNull
     @Override
-    //뷰홀더 객체 생성해서 리턴
+    // 뷰홀더 객체 생성해서 리턴
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         Context context = parent.getContext();
         //일부 뷰를 메모리 상에 객체화 할때 layoutinflatoer 클래스 사용
@@ -82,7 +82,7 @@ public class ListAdapterOld extends RecyclerView.Adapter<ListAdapterOld.ViewHold
     }
 
     @Override
-    //포지션에 해당하는 데이터를 뷰홀더의 아이템뷰에 표시
+    // 포지션에 해당하는 데이터를 뷰홀더의 아이템뷰에 표시
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
         ApItem ap_item = ap_items.get(position);
 
@@ -125,7 +125,7 @@ public class ListAdapterOld extends RecyclerView.Adapter<ListAdapterOld.ViewHold
 
     }
 
-    //전체 데이터 갯수 리턴
+    // 전체 데이터 갯수 리턴
     @Override
     public int getItemCount() {
         return ap_items.size();
