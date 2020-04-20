7574fc2ca7f574ec8d98dc82799c6e9dfac6f112
Nevaland <deanh0716@gmail.com>
2020-04-21 01:37:12 +0900

    Add secure light branch and element values

--- app/src/main/java/com/example/maplefi/MoreActivity.java
+++ app/src/main/java/com/example/maplefi/MoreActivity.java
@@ -25,6 +25,21 @@ public class MoreActivity extends AppCompatActivity {
         final TextView textViewSsid = (TextView) findViewById(R.id.tv_ssid);
         textViewSsid.setText(ap_item.getSsid());
 
+        final ImageView imgViewLight = (ImageView) findViewById(R.id.img_color);
+        int secure_level = ap_item.getSecLevel();
+        switch (secure_level){
+            case 1:
+                imgViewLight.setImageResource(R.drawable.red);
+                break;
+            case 2:
+                imgViewLight.setImageResource(R.drawable.orange);
+                break;
+            case 3:
+                imgViewLight.setImageResource(R.drawable.green);
+                break;
+            default:
+                imgViewLight.setImageResource(R.drawable.red);
+        }
         final ImageView imgRssi = (ImageView) findViewById(R.id.img_rssiDegree);
         int rssi_level = ap_item.getRssiLevel();
         switch (rssi_level){
@@ -41,7 +56,6 @@ public class MoreActivity extends AppCompatActivity {
                 imgRssi.setImageResource(R.drawable.wifi_x);
         }
 
-        ImageButton imgButtonMoreinf = (ImageButton) findViewById(R.id.imgb_moreinf) ;
         ImageButton imgButtonConnect = (ImageButton) findViewById(R.id.imgb_connect) ;
         imgButtonConnect.setOnClickListener(new Button.OnClickListener() {
             @Override
@@ -50,12 +64,45 @@ public class MoreActivity extends AppCompatActivity {
             }
         });
 
+        // -------------------------------------------------------------//
+        final TextView textViewCap1 = (TextView) findViewById(R.id.tv_cap1);
+        textViewCap1.setText("CAP1 : ");
+
+        final TextView textViewCap2 = (TextView) findViewById(R.id.tv_cap2);
+        textViewCap2.setText("CAP2 : ");
+
+        final TextView textViewCap3 = (TextView) findViewById(R.id.tv_cap3);
+        textViewCap3.setText("CAP3 : ");
+
+        final TextView textViewCap4 = (TextView) findViewById(R.id.tv_cap4);
+        textViewCap4.setText("CAP4 : ");
+
+
         final TextView textViewSecScore = (TextView) findViewById(R.id.tv_sec_score);
+        textViewSecScore.setText(Integer.toString(ap_item.getSecScore())+" 점");
+
+//        final ImageView imgViewLightSet = (ImageView) findViewById(R.id.img_sec_score);
+//        imgViewLight.setImageResource(R.drawable.);
+
+        final TextView textViewSpeedScore = (TextView) findViewById(R.id.tv_speed_score);
+        textViewSpeedScore.setText(Integer.toString(ap_item.getRssiScore()) + " dBm");
+
+        ImageButton imgButtonDetail = (ImageButton) findViewById(R.id.imgb_detail) ;
+        imgButtonDetail.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Extend Detail Info
+
+
+            }
+        });
+
+//        final TextView textViewSecScore = (TextView) findViewById(R.id.tv_sec_score);
 //        textViewSecScore.setText("TEST eap type:"+Integer.toString(ap_item.getEapType()));
-        textViewSecScore.setText(Integer.toString(ap_item.getSecScore()));
-        final TextView textViewSpdScore = (TextView) findViewById(R.id.tv_speed_score);
+//        textViewSecScore.setText(Integer.toString(ap_item.getSecScore()));
+//        final TextView textViewSpdScore = (TextView) findViewById(R.id.tv_speed_score);
 //        textViewSpdScore.setText("TEST caps:"+ap_item.getCaps());
-        textViewSpdScore.setText(Integer.toString(ap_item.getRssiScore()));
+//        textViewSpdScore.setText(Integer.toString(ap_item.getRssiScore()));
 
         TextView textViewReport = (TextView) findViewById(R.id.tv_detail_desc);
         //보안방식에 따른 레포트
@@ -68,13 +115,11 @@ public class MoreActivity extends AppCompatActivity {
             Log.d("test", "onCreate: wpa");
             if(ap_item.getCaps().contains("TKIP")){
                 Log.d("test", "onCreate: wpa-tkip");
-                textViewReport.setText(getString(R.string.wpa_report)+" "
-                        + getString(R.string.tkip_report));
+                textViewReport.setText(getString(R.string.wpa_report) + " " + getString(R.string.tkip_report));
                 Log.d("test", "onCreate: "+textViewReport.toString());
             }else if(ap_item.getCaps().contains("PSK")){
                 Log.d("test", "onCreate: wpa-psk");
-                textViewReport.setText(getString(R.string.wpa_report) + ""
-                        + getString(R.string.psk_report));
+                textViewReport.setText(getString(R.string.wpa_report) + "" + getString(R.string.psk_report));
                 Log.d("test", "onCreate: "+textViewReport.toString());
             }
         }else if(ap_item.getCaps().contains("WPA2")){
