f797d8219f1a0e4d03ff6bc734e62f572afc7d1b
Nevaland <deanh0716@gmail.com>
2020-04-22 00:34:20 +0900

    Remove ap sample

--- app/src/main/java/com/example/maplefi/MainActivity.java
+++ app/src/main/java/com/example/maplefi/MainActivity.java
@@ -160,7 +160,7 @@ public class MainActivity extends AppCompatActivity implements MainActivityNavig
         updateNowAp();
 
         // - 테스트 샘플
-        addItem("CJWIFI_9C1A","[WPA-PSK-CCMP+TKIP]",-50,-1);
+//        addItem("CJWIFI_9C1A","[WPA-PSK-CCMP+TKIP]",-50,-1);
 //        addItem("IPTIME","[WEP][ESS]",-80);
 
         WifiReceiver receiverWifi = new WifiReceiver();
