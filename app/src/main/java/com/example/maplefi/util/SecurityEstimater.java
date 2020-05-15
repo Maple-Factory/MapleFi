c511ae5b5102485d25d3bb4504b3a08265d99eed
pororili <xiozioll@gmail.com>
2020-05-15 21:17:25 +0900

    Solved the Security score problem

--- app/src/main/java/com/example/maplefi/util/SecurityEstimater.java
+++ app/src/main/java/com/example/maplefi/util/SecurityEstimater.java
@@ -17,19 +17,28 @@ public class SecurityEstimater {
     //test 용 임의 정보들
 //    ApItem ap_Item = null;
     String TAG = "SecurityEstimater";
-    int scores[] = new int[5];//score array
+    int scores[] = new int[5];   /* = new int[5];*///score array
     //0 : 총점,  1 : pw, 2 : packet,packet rule , 3 : packet snif, 4 : ssid,
 
     public SecurityEstimater(String ap_name, String capabilities, int eap_type) {
         Log.d(TAG, "SecurityEstimater: after apItem set");
 //        int scores[] = new int[5];   //score array
-        Log.d(TAG, "SecurityEstimater: 계산 전 " +scores[0]);
+        for(int i=0; i<5; i++){
+            scores[i] = 0;
+            Log.d(TAG, "SecurityEstimater: 초기화 후 값 확인 "+i+"번째"+scores[i]);
+        }
 
         checkPassword(capabilities);
         checkSsid(ap_name);
         checkAsign(capabilities);
         checkEnc(capabilities);
 //        checkPacketsnif(eap_type);
+
+        for(int i=1; i<5; i++){
+            scores[0]+=scores[i];
+            Log.d(TAG, "SecurityEstimater: checkAsign 후 "+i+"번째"+scores[i]);
+        }
+
 
         Log.d(TAG, "SecurityEstimater: final = " + scores[0]);
     }
@@ -42,22 +51,6 @@ public class SecurityEstimater {
 ////            ap_Item.addSec_Score(50);//임의
 //            Log.d(TAG, "pw_checker: pwtype = " + scores);
 
-            if (capabilities.contains("WPA")) {
-                Log.d(TAG, "pw_checker: wpa");
-//                ap_Item.addSec_Score(20);
-                scores[1] += 20;
-                scores[3] += 20;
-                Log.d(TAG, "pw_checker: score[1] = " + scores[1]);
-            }
-            if (capabilities.contains("WPA2")) {
-                Log.d(TAG, "pw_checker: wpa2");
-//                capabilities.addSec_Score(40);
-                scores[1] += 40;
-                scores[3] += 30;
-
-                Log.d(TAG, "pw_checker: score[1] = " + scores[1]);
-
-            }
             if (capabilities.contains("WEP")) {
                 Log.d(TAG, "pw_checker: wep");
 //                ap_Item.addSec_Score(10);
@@ -66,10 +59,35 @@ public class SecurityEstimater {
 
                 Log.d(TAG, "pw_checker: " + scores[1]);
             }
-            scores[0] += scores[1];// 총점에 개별점수 더하기
-            scores[0] += scores[3];//총점에 패킷도청점수 더하기
-            Log.d(TAG, "pw_checker: score[1] = " + scores[1]);
-            Log.d(TAG, "pw_checker: score[0] = "+ scores[0]);
+            else if (capabilities.contains("WPA-")) {
+                Log.d(TAG, "pw_checker: wpa");
+//                ap_Item.addSec_Score(20);
+                scores[1] += 20;
+                scores[3] += 20;
+                Log.d(TAG, "pw_checker: score[1] = " + scores[1]);
+            }
+            else if (capabilities.contains("WPA2")) {
+                Log.d(TAG, "pw_checker: wpa2");
+//                capabilities.addSec_Score(40);
+                scores[1] += 40;
+                scores[3] += 30;
+
+                Log.d(TAG, "pw_checker: score[1] = " + scores[1]);
+
+            }
+            else if (capabilities.contains("RSN")) {
+                Log.d(TAG, "pw_checker: wpa2");
+//                capabilities.addSec_Score(40);
+                scores[1] += 40;
+                scores[3] += 30;
+
+                Log.d(TAG, "pw_checker: score[1] = " + scores[1]);
+
+            }
+
+            for(int i=0; i<5; i++){
+                Log.d(TAG, "SecurityEstimater: checkpassword 후 "+i+"번째"+scores[i]);
+            }
 
         }
     }
@@ -85,6 +103,10 @@ public class SecurityEstimater {
             Log.d(TAG, "ssid_checker: 숨김모드 아님");
         }
         scores[0] += scores[4];
+        for(int i=0; i<5; i++){
+            Log.d(TAG, "SecurityEstimater: ssid 후 "+i+"번째"+scores[i]);
+        }
+
     }
 
     //프로토콜 방식 판별
@@ -95,7 +117,9 @@ public class SecurityEstimater {
 //            Log.d(TAG, "packet_checker: " + ap_Item.getSecScore());
             scores[2] += 30;
             scores[3] += 30;
-            Log.d(TAG, "packet_checker: "+ scores);
+            for(int i=0; i<5; i++){
+                Log.d(TAG, "SecurityEstimater: checkAsign 후 "+i+"번째"+scores[i]);
+            }
 
         } else if (capabilities.contains("EAP")) {
             Log.d(TAG, "packet_checker: protocol eap");
@@ -103,23 +127,26 @@ public class SecurityEstimater {
 //            Log.d(TAG, "packet_checker: " + ap_Item.getSecScore());
             scores[2] += 40;
             scores[3] += 40;
-            Log.d(TAG, "packet_checker: "+ scores);
-            //just
+            for(int i=0; i<5; i++){
+                Log.d(TAG, "SecurityEstimater: checkAsign 후 "+i+"번째"+scores[i]);
+            }
 
         } else {
             Log.d(TAG, "packet_checker: not in our packet case");
             Log.d(TAG, "packet_checker: "+ scores);
         }
-        scores[0] += scores[2];
-        scores[0] += scores[3];
-        Log.d(TAG, "packet_checker: "+ scores);
+
+        for(int i=0; i<5; i++){
+            Log.d(TAG, "SecurityEstimater: checkAsign 후 "+i+"번째"+scores[i]);
+        }
+
     }
 
 
     //패킷 암호화 규칙 체크
     public void checkEnc(String capabilities){
        if (capabilities.contains("CCMP")) {
-           Log.d(TAG, "packetRule_checker: ccmp");
+           Log.d(TAG, "check_Enc: ccmp");
 //           ap_Item.addSec_Score(20);
 //           Log.d(TAG, "packetRule_checker: grade " + ap_Item.getSecScore());
            scores[2] += 30;
@@ -130,13 +157,15 @@ public class SecurityEstimater {
 //            Log.d(TAG, "packet_checker: " + ap_Item.getSecScore());
                scores[2] += 20;
                scores[3] += 20;
-               Log.d(TAG, "packet_checker: "+ scores);
+               Log.d(TAG, "packet_checker: "+ scores[2]);
        } else {
            Log.d(TAG, "packetRule_checker: not in packetrule case");
        }
-       scores[0] += scores[2];
-       scores[0] += scores[3];
-        Log.d(TAG, "packetRule_checker: "+ scores[0]);
+
+        for(int i=0; i<5; i++){
+            Log.d(TAG, "SecurityEstimater: checkEnc 후 "+i+"번째"+scores[i]);
+        }
+
     }
 
     //도청 난이도 판별
