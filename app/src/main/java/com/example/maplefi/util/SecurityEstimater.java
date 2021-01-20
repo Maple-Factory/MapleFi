5abdcb87e6c5510495e3d7f19f2a77690f1877cb
pororili <xiozioll@gmail.com>
2021-01-20 10:32:42 +0900

    Remark the logs

--- app/src/main/java/com/example/maplefi/util/SecurityEstimater.java
+++ app/src/main/java/com/example/maplefi/util/SecurityEstimater.java
@@ -11,10 +11,10 @@ public class SecurityEstimater {
     //0 : 총점,  1 : pw, 2 : packet,packet rule , 3 : packet snif, 4 : ssid,
 
     public SecurityEstimater(String ap_name, String capabilities, int eap_type) {
-        Log.d(TAG, "SecurityEstimater: after apItem set");
+//        Log.d(TAG, "SecurityEstimater: after apItem set");
         for(int i=0; i<5; i++){
             scores[i] = 0;
-            Log.d(TAG, "SecurityEstimater: 초기화 후 값 확인 "+i+"번째"+scores[i]);
+//            Log.d(TAG, "SecurityEstimater: 초기화 후 값 확인 "+i+"번째"+scores[i]);
         }
 
         checkPassword(capabilities);
@@ -24,50 +24,50 @@ public class SecurityEstimater {
 
         for(int i=1; i<5; i++){
             scores[0]+=scores[i];
-            Log.d(TAG, "SecurityEstimater: checkAsign 후 "+i+"번째"+scores[i]);
+//            Log.d(TAG, "SecurityEstimater: checkAsign 후 "+i+"번째"+scores[i]);
         }
 
 
-        Log.d(TAG, "SecurityEstimater: final = " + scores[0]);
+//        Log.d(TAG, "SecurityEstimater: final = " + scores[0]);
     }
 
     //포지션에 해당하는 리스트의 로그온 암호적용, 암호화 방식평가 여부
     public void checkPassword(String capabilities) {
         if (!(capabilities.toUpperCase().isEmpty())) {
-            Log.d(TAG, "pw_checker: pwtype 존재");
+//            Log.d(TAG, "pw_checker: pwtype 존재");
 
             if (capabilities.contains("WEP")) {
-                Log.d(TAG, "pw_checker: wep");
+//                Log.d(TAG, "pw_checker: wep");
                 scores[1] += 10;
                 scores[3] += 0;
 
-                Log.d(TAG, "pw_checker: " + scores[1]);
+//                Log.d(TAG, "pw_checker: " + scores[1]);
             }
             else if (capabilities.contains("WPA-")) {
-                Log.d(TAG, "pw_checker: wpa");
+//                Log.d(TAG, "pw_checker: wpa");
                 scores[1] += 20;
                 scores[3] += 20;
-                Log.d(TAG, "pw_checker: score[1] = " + scores[1]);
+//                Log.d(TAG, "pw_checker: score[1] = " + scores[1]);
             }
             else if (capabilities.contains("WPA2")) {
-                Log.d(TAG, "pw_checker: wpa2");
+//                Log.d(TAG, "pw_checker: wpa2");
                 scores[1] += 40;
                 scores[3] += 30;
 
-                Log.d(TAG, "pw_checker: score[1] = " + scores[1]);
+//                Log.d(TAG, "pw_checker: score[1] = " + scores[1]);
 
             }
             else if (capabilities.contains("RSN")) {
-                Log.d(TAG, "pw_checker: wpa2");
+//                Log.d(TAG, "pw_checker: wpa2");
                 scores[1] += 40;
                 scores[3] += 30;
 
-                Log.d(TAG, "pw_checker: score[1] = " + scores[1]);
+//                Log.d(TAG, "pw_checker: score[1] = " + scores[1]);
 
             }
 
             for(int i=0; i<5; i++){
-                Log.d(TAG, "SecurityEstimater: checkpassword 후 "+i+"번째"+scores[i]);
+//                Log.d(TAG, "SecurityEstimater: checkpassword 후 "+i+"번째"+scores[i]);
             }
 
         }
@@ -75,16 +75,16 @@ public class SecurityEstimater {
 
     //ssid check
     public void checkSsid(String ap_name) {
-        Log.d(TAG, "ssid_checker: 여기");
+//        Log.d(TAG, "ssid_checker: 여기");
         if (Objects.equals(ap_name, "숨겨진 네트워크")) {//숨김모드일경우
-            Log.d(TAG, "ssid_checker: 숨김모드");
+//            Log.d(TAG, "ssid_checker: 숨김모드");
             scores[4] += 60;
         } else {
-            Log.d(TAG, "ssid_checker: 숨김모드 아님");
+//            Log.d(TAG, "ssid_checker: 숨김모드 아님");
         }
         scores[0] += scores[4];
         for(int i=0; i<5; i++){
-            Log.d(TAG, "SecurityEstimater: ssid 후 "+i+"번째"+scores[i]);
+//            Log.d(TAG, "SecurityEstimater: ssid 후 "+i+"번째"+scores[i]);
         }
 
     }
@@ -92,28 +92,28 @@ public class SecurityEstimater {
     //프로토콜 방식 판별
     public void checkAsign(String capabilities) {
         if (capabilities.contains("PSK")) {
-            Log.d(TAG, "packet_checker: protocol psk");
+//            Log.d(TAG, "packet_checker: protocol psk");
             scores[2] += 30;
             scores[3] += 30;
             for(int i=0; i<5; i++){
-                Log.d(TAG, "SecurityEstimater: checkAsign 후 "+i+"번째"+scores[i]);
+//                Log.d(TAG, "SecurityEstimater: checkAsign 후 "+i+"번째"+scores[i]);
             }
 
         } else if (capabilities.contains("EAP")) {
-            Log.d(TAG, "packet_checker: protocol eap");
+//            Log.d(TAG, "packet_checker: protocol eap");
             scores[2] += 40;
             scores[3] += 40;
             for(int i=0; i<5; i++){
-                Log.d(TAG, "SecurityEstimater: checkAsign 후 "+i+"번째"+scores[i]);
+//                Log.d(TAG, "SecurityEstimater: checkAsign 후 "+i+"번째"+scores[i]);
             }
 
         } else {
-            Log.d(TAG, "packet_checker: not in our packet case");
-            Log.d(TAG, "packet_checker: "+ scores);
+//            Log.d(TAG, "packet_checker: not in our packet case");
+//            Log.d(TAG, "packet_checker: "+ scores);
         }
 
         for(int i=0; i<5; i++){
-            Log.d(TAG, "SecurityEstimater: checkAsign 후 "+i+"번째"+scores[i]);
+//            Log.d(TAG, "SecurityEstimater: checkAsign 후 "+i+"번째"+scores[i]);
         }
 
     }
@@ -122,20 +122,20 @@ public class SecurityEstimater {
     //패킷 암호화 규칙 체크
     public void checkEnc(String capabilities){
        if (capabilities.contains("TKIP")) {
-           Log.d(TAG, "check_Enc: TKIP");
+//           Log.d(TAG, "check_Enc: TKIP");
            scores[2] += 20;
            scores[3] += 20;
        }else if (capabilities.contains("CCMP")) {
-               Log.d(TAG, "packet_checker: protocol CCMP");//wep에서 사용하는 암호화 방식
+//               Log.d(TAG, "packet_checker: protocol CCMP");//wep에서 사용하는 암호화 방식
                scores[2] += 40;
                scores[3] += 40;
-               Log.d(TAG, "packet_checker: "+ scores[2]);
+//               Log.d(TAG, "packet_checker: "+ scores[2]);
        } else {
-           Log.d(TAG, "packetRule_checker: not in packetrule case");
+//           Log.d(TAG, "packetRule_checker: not in packetrule case");
        }
 
         for(int i=0; i<5; i++){
-            Log.d(TAG, "SecurityEstimater: checkEnc 후 "+i+"번째"+scores[i]);
+//            Log.d(TAG, "SecurityEstimater: checkEnc 후 "+i+"번째"+scores[i]);
         }
 
     }
